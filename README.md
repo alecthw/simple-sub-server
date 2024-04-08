585fd37a4deb6d957e1bafa9678ce2421e23b6b7
alecthw <alecthw@163.com>
2024-04-08 15:05:10 +0800

    1. add file redirect 2. add subconverter redirect

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ git clone https://github.com/alecthw/simple-sub-server.git
 cd simple-sub-server
 export GOOS=linux GOARCH=amd64 # 可选，交叉编译
 go build -o sub-server
-
 ```
 
 ## 使用
@@ -27,7 +26,7 @@ go build -o sub-server
 ### 运行
 
 ```bash
-./sub-server -dir /path/to/workDir -host 127.0.0.1:8080
+./sub-server -dir /path/to/workDir -host 127.0.0.1:8080 -subcnv "http://127.0.0.1:25500"
 ```
 
 ### 工作目录示例
@@ -41,10 +40,29 @@ go build -o sub-server
 │   │   ├── Stash.yaml
 │   │   └── Surge.conf
 │   └── 58cfbff0-18c8-1f7d-400a-ba07a305b1e6
+│       ├── clash.ini
 │       ├── ClashMeta.yaml
 │       └── ClashMetaOnlyCN.yaml
 └── sub-server
 ```
+
+#### 文件重定向支持
+
+重定向的判断是判断文件内容以 `[Redirect]` 开头，内容是 `ini` 格式。
+
+```ini
+[Redirect]
+file=clash.ini # 必填
+uuid=58cfbff0-18c8-1f7d-400a-ba07a305b1e6 # 选填，不填则沿用原 uuid
+```
+
+PS: 重定向视为可信输入，不再校验外部输入逻辑，此时 uuid 可为任意字符串。
+
+#### subconverter 调用支持
+
+subconverter 服务器默认地址 `http://127.0.0.1:25500`，可以通过启动参数 `-subcnv` 修改。
+
+当前文件后缀为 `ini` 时，则认为是 subconverter 配置文件，将调用 subconverter 服务。文件内容写法与 [subconverter 配置档案](https://github.com/tindy2013/subconverter/blob/master/README-cn.md#%E9%85%8D%E7%BD%AE%E6%A1%A3%E6%A1%88)相同。调用时，是将配置档拆解成独立参数，然后调用的。
 
 ### systemd 服务
 
--- main.go
+++ main.go
@@ -3,31 +3,41 @@ package main
 import (
 	"flag"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/alecthw/sub-server/log"
+	"gopkg.in/ini.v1"
 
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
 var (
-	workDir string
-	host    string
+	workDir    string
+	host       string
+	subconvUrl string
+
+	client *resty.Client
 )
 
 func init() {
 	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-	flag.StringVar(&workDir, "dir", dir, "The wroking directory. Default executable file directory.")
 
+	flag.StringVar(&workDir, "dir", dir, "The wroking directory. Default executable file directory.")
 	flag.StringVar(&host, "host", ":8080", "Http server bind host. Default \":8080\"")
+	flag.StringVar(&subconvUrl, "subcnv", "http://127.0.0.1:25500", "subconverter server bind host. Default \"http://127.0.0.1:25500\"")
+
 	flag.Parse()
 
 	gin.SetMode("release")
+
+	client = resty.New()
 }
 
 func main() {
@@ -42,26 +52,37 @@ func main() {
 		uuid := c.Param("uuid")
 		file := c.Param("file")
 
+		// check uuid and file name valid
 		if !isValidUUID(uuid) || !isPathSecure(file) {
 			c.String(403, "Forbidden")
 			return
 		}
 
+		// check user path exist
 		userPath := filepath.Join(workDir, "sub", uuid)
 		if !pathExists(userPath) {
 			c.String(404, "Not found")
 			return
 		}
 
+		// check file exist
 		subFilePath := filepath.Join(userPath, file)
 		if !pathExists(subFilePath) {
 			c.String(404, "Not found")
 			return
 		}
 
-		fileContent, err := os.ReadFile(subFilePath)
+		// get file content
+		fileContent, subFilePath, err := getFileContent(uuid, file)
 		if err != nil {
 			c.String(404, "Not found")
+			return
+		}
+
+		// if file is ini, redirect to subconverter
+		fileExt := path.Ext(subFilePath)
+		if fileExt == ".ini" {
+			c.Data(200, "text/plain; charset=UTF-8", getSubconv(fileContent))
 			return
 		}
 
@@ -71,11 +92,54 @@ func main() {
 	r.Run(host)
 }
 
+// get file content and check if it is a redirect file
+func getFileContent(uuid string, file string) ([]byte, string, error) {
+	subFilePath := filepath.Join(workDir, "sub", uuid, file)
+	fileContent, err := os.ReadFile(subFilePath)
+	if err != nil {
+		return nil, subFilePath, err
+	}
+
+	fileStr := string(fileContent)
+	if strings.HasPrefix(fileStr, "[Redirect]") {
+		cfgs, _ := ini.Load((fileContent))
+		nextFile := cfgs.Section("Redirect").Key("file").String()
+		nextUuid := uuid
+		if cfgs.Section("Redirect").HasKey("uuid") {
+			nextUuid = cfgs.Section("Redirect").Key("uuid").String()
+		}
+		fileContent, subFilePath, _ = getFileContent(nextUuid, nextFile)
+		return fileContent, subFilePath, nil
+	}
+
+	return fileContent, subFilePath, nil
+}
+
+// get subconverter response
+func getSubconv(fileContent []byte) []byte {
+	cfgs, err := ini.Load((fileContent))
+	if err != nil {
+		return []byte("")
+	}
+
+	resp, err := client.R().
+		SetQueryParams(cfgs.Section("Profile").KeysHash()).
+		Get(subconvUrl + "/sub")
+
+	if err != nil {
+		return []byte("")
+	}
+
+	return resp.Body()
+}
+
+// check if a string is a valid uuid
 func isValidUUID(u string) bool {
 	_, err := uuid.Parse(u)
 	return err == nil
 }
 
+// check if a path is secure
 func isPathSecure(filePath string) bool {
 	if strings.Contains(filePath, "..") || strings.Contains(filePath, "/") || strings.Contains(filePath, "\\") {
 		return false
@@ -84,6 +148,7 @@ func isPathSecure(filePath string) bool {
 	}
 }
 
+// check if a path exists
 func pathExists(filePath string) bool {
 	_, err := os.Stat(filePath)
 	if err != nil {
